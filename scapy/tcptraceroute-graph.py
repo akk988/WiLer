1b5206d4b4fbc77034e94e6b3fb065599c051644
akk988 <ak.khalil.1988@gmail.com>
2020-12-01 18:10:35 +0100

    Add new module to measure and plot a graph of TCP trace route - svg saved in /tmp/..

--- scapy/tcptraceroute-graph.py
+++ scapy/tcptraceroute-graph.py
@@ -1,20 +1,4 @@
 from scapy.all import*
 
 res, unans = traceroute(["www.microsoft.com","www.cisco.com","www.yahoo.com"],dport=[80,443],maxttl=20,retry=-2)
-res.graph(target="> /tmp/from-script.svg")
-#p=send(IP(dst="192.168.0.1")/ICMP())
-#dest = "192.168.0.105"
-#pkt = []
-#for i in range(1,50):
-#    a = IP(dst=dest) / TCP(flags="S", seq=i, sport=65000, dport=55556)
-#    pkt.append(a)
-
-#ans, unans = sr(pkt, filter="host {0}".format(dest), inter=0, timeout=1)
-
-#print("scapy version: {}".format(conf.version))
-
-#for pkt in ans:
-#    sent = pkt[0]
-#    received = pkt[1]
-#    res = (received.time - sent.sent_time) * 1000
-#    print(res)
+res.graph(target="> /tmp/tcpTraceRoute-Graph.svg")
