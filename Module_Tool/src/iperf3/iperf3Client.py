cca9400a99c6beecf818859275af7b2556f45134
akk <IPA>
2020-12-30 21:54:35 +0100

    Add new module, iperf3, to the Module_Tool

--- Module_Tool/src/iperf3/iperf3Client.py
+++ Module_Tool/src/iperf3/iperf3Client.py
@@ -10,14 +10,18 @@ global server_ip, client_ip, path, wNpass
 server_ip = "192.168.0.10x"
 client_ip = "192.168.0.10y"
 wNpass = "remote-host-password"
-wN1_ip = "/home/pi/myFolder
+wN1_ip = "/home/pi/someFolder
 
-iperf3_client:
+def iperf3_server:
 
+    print ('== Starting iperf3 Server on localhost  ==')
     # Uncomment to start the iperf3 server on remote host with IP= server_ip 
     #bws = subprocess.Popen(["sudo","sshpass", "-p", wNpass, "ssh", "-o", SHCK, server_ip, "&&", "sudo", "iperf3", "-s"], stdout=subprocess.PIPE)
-   
-    print ('== Starting iperf3 Client on localhost  ==')
+
+
+def iperf3_client:
+
+
     bw = subprocess.Popen(["sudo", "iperf3", "-c", server_ip, "-i","1","-t","10",">", "bandwidth-client.txt"], stdout=subprocess.PIPE)
     bw = pa.stdout.read()
 
@@ -34,4 +38,8 @@ iperf3_client:
     
     #global h1bw
     h1bw = float(hli1[0:4])
+    #TODO filter the results when kbps and handle converting the values
     print ('== Bandwidth from host 1 = ', h1bw, " Mbps")
+
+iperf3_server()
+iperf3_client()
